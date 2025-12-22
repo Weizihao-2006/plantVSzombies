55a5f77191615638054249c06e4b0c7700419b36
user-jack222 <2455337887@qq.com>
2025-12-22 16:21:25 +0800

    尝试合并分支到main

--- Classes/plant/WallNut.h
+++ Classes/plant/WallNut.h
@@ -12,12 +12,6 @@ public:
     // 重写关键接口：根据血量更新视觉状态
     virtual void updateVisualByHealth() override;
 
-public:
-    enum class NutState {
-        Health,     // 100% - 70%
-        Damaged,    // 70% - 30%
-        Critical    // 30% - 0%
-    };
 
 private:
     // 记录当前处于哪种破损阶段，避免重复切换动画造成性能浪费
